e2034b6bb79a9ecdf029a543aee82e4dffeafc8e
Alex Stepanian <alexstepanian11@gmail.com>
2022-04-27 12:17:15 -0700

    add loading component

---
+++ src/Loading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <section className="loading-section">
+      <h2>
+        Loading
+        <span className="dot1">.</span>
+        <span className="dot2">.</span>
+        <span className="dot3">.</span>
+      </h2>
+    </section>
+  );
+};
+
+export default Loading;
