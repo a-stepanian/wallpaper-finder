d11c3bdd562e55cd9cab55e5e3d371fc8ccfa975
Alex Stepanian <alexstepanian11@gmail.com>
2022-04-27 12:19:03 -0700

    add grid column and viewport states to dynamically change on window resize

--- src/Photos.js
+++ src/Photos.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Photos = ({ photos, setSelectedPhoto, setBigPic, orientation }) => {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+  const [columnSchema, setColumnSchema] = useState(null);
+
+  // Set the viewport width state when the window is resized
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  //
+
+  // Open the enlarged photo modal when the photo is clicked
   const handleClick = (photo) => {
     setSelectedPhoto(photo);
     setBigPic(true);
   };
+
+  useEffect(() => {
+    if (orientation === "portrait" && viewportWidth < 1000) {
+      setColumnSchema("33% 33% 33%");
+    }
+    if (orientation === "portrait" && viewportWidth >= 1000) {
+      setColumnSchema("25% 25% 25% 25%");
+    }
+    if (orientation === "landscape" && viewportWidth < 1000) {
+      setColumnSchema("50% 50%");
+    }
+    if (orientation === "landscape" && viewportWidth >= 1000) {
+      setColumnSchema("33% 33% 33%");
+    }
+  }, [viewportWidth]);
+
   return (
     <section
       className="photo-section"
       style={{
-        gridTemplateColumns:
-          orientation === "portrait" ? "33% 33% 33%" : "50% 50%",
+        gridTemplateColumns: columnSchema,
       }}
     >
       {photos.map((photo, index) => {
